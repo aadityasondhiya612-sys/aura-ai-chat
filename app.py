09e4e0b2789710687d55e054b7da7a758f822ca4
aadityasondhiya612-sys <aadityasondhiya612@gmail.com>
2026-04-08 12:48:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_msg = request.json['message']
+    reply = "Tumne likha: " + user_msg
+    return jsonify({"reply": reply})
+
+if __name__ == "__main__":
+    app.run()
